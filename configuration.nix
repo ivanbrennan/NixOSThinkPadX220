3e3d2575c5fe5432679dcdc83453c8635050a4e2
ivanbrennan <ivan.brennan@gmail.com>
2017-10-06 23:00:38 -0400

    programs.bash.enableCompletion

--- configuration.nix
+++ configuration.nix
@@ -43,7 +43,6 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
-    bashCompletion
     chromium
     git
     tree
@@ -55,7 +54,7 @@
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.bash.enableCompletion = true;
+  programs.bash.enableCompletion = true;
   # programs.mtr.enable = true;
   # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
