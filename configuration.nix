85cbfa475208e647801c434dde11cf21779bf360
ivanbrennan <ivan.brennan@gmail.com>
2017-10-15 08:56:34 -0400

    ag

--- configuration.nix
+++ configuration.nix
@@ -42,6 +42,7 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
+    ag
     aspell
     aspellDicts.en
     chromium
