80e74de87f63175d92aec567004da4bac656e3c6
ivanbrennan <ivan.brennan@gmail.com>
2017-12-04 21:38:22 -0500

    clean up configuration

--- configuration.nix
+++ configuration.nix
@@ -21,26 +21,17 @@
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+  boot.loader.grub.device = "/dev/sda";
 
-  networking.hostName = "nixosX220"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "littleThinkPad";
 
-  # Select internationalisation properties.
   i18n = {
     consoleKeyMap = "us";
     defaultLocale = "en_US.UTF-8";
   };
 
-  # Set your time zone.
   time.timeZone = "America/New_York";
 
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
     ag
     alacritty
@@ -74,48 +65,19 @@
   nix.gc.automatic = true;
   nix.gc.dates = "03:15";
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
   programs.bash.enableCompletion = true;
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
   fonts.fonts = [
     pkgs.source-code-pro
     pkgs.emacs-all-the-icons-fonts
   ];
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   services.emacs.enable = true;
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.xserver.layout = "us";
   services.xserver.xkbOptions = "shift:both_capslock,caps:ctrl_modifier";
-
-  # Better 3D performance than the X.org drivers
-  # services.xserver.videoDrivers = [ "nvidia" ];
-
-  # Enable touchpad support.
-  # services.xserver.libinput.enable = false;
-  # services.xserver.synaptics.enable = true;
-  # services.xserver.synaptics.twoFingerScroll = true;
-
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
 
   # Gnome desktop
   services.xserver.desktopManager = {
@@ -131,7 +93,6 @@
     default = "gnome3";
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.extraUsers.ivan = {
     isNormalUser = true;
     uid = 1000;
@@ -149,5 +110,4 @@
   system.stateVersion = "17.09"; # Did you read the comment?
 
   system.autoUpgrade.enable = true;
-
 }
